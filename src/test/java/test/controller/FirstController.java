0d2dcd1da75ab50d187a389497ad6ee0e468e9fa
dingtao <d.t.ubest@gmail.com>
2013-01-25 15:33:40 +0800

    上产了一个单元测试的类

---
+++ src/test/java/test/controller/FirstController.java
@@ -0,0 +1,14 @@
+package test.controller;
+
+import com.me.web.servlet.result.Result;
+
+/**
+ * User: t.ding
+ * Date: 12-12-8
+ */
+public class FirstController {
+    public Result logic3(String para_text) {
+        System.out.println(para_text);
+        return null;
+    }
+}

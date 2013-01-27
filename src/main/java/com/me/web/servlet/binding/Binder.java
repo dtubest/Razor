e4979342f1ecc370644c5134e5c98dafb54c5858
dingtao <d.t.ubest@gmail.com>
2013-01-27 10:39:23 +0800

    对binder进行了优化，不会在每次请求binder时都去forName，写了点javadoc

--- src/main/java/com/me/web/servlet/binding/Binder.java
+++ src/main/java/com/me/web/servlet/binding/Binder.java
@@ -20,15 +20,15 @@ public abstract class Binder<T> {
     public static final DoubleBinder doubleBinder = new DoubleBinder();
     public static final BooleanBinder booleanBinder = new BooleanBinder();
 
-    private static Map<Class, Binder> binderMap = new HashMap<Class, Binder>();
+    private static Map<String, Binder> binderMap = new HashMap<String, Binder>();
 
     static {
-        binderMap.put(String.class, stringBinder);
-        binderMap.put(Integer.class, integerBinder);
-        binderMap.put(Long.class, longBinder);
-        binderMap.put(Float.class, floatBinder);
-        binderMap.put(Double.class, doubleBinder);
-        binderMap.put(Boolean.class, booleanBinder);
+        binderMap.put(String.class.getName(), stringBinder);
+        binderMap.put(Integer.class.getName(), integerBinder);
+        binderMap.put(Long.class.getName(), longBinder);
+        binderMap.put(Float.class.getName(), floatBinder);
+        binderMap.put(Double.class.getName(), doubleBinder);
+        binderMap.put(Boolean.class.getName(), booleanBinder);
     }
 
     public T get(String paramName, HttpServletRequest request, Mapping mapping) {
@@ -50,18 +50,26 @@ public abstract class Binder<T> {
         return cast(parameter);
     }
 
+    /**
+     * 根据类型的全名（java.lang.String）来获取类型binder，如果是不支持的类型，
+     * 返回UnsupportedTypeBinder对象，所以这个方法返回值永远不为null
+     *
+     * @param typeName 类型的全名
+     * @return 目标类型binder或者UnsupportedTypeBinder对象
+     */
     public static Binder<?> binderOf(String typeName) {
-        Binder binder;
-        try {
-            binder = binderMap.get(Class.forName(typeName));
-        } catch (ClassNotFoundException e) {
-            binder = null;
-        }
+        Binder binder = binderMap.get(typeName);
         if (null != binder)
             return binder;
 
-        return new InvalidTypeBinder();
+        return new UnsupportedTypeBinder();
     }
 
+    /**
+     * 这个方法用来提供类型转型,由扩展的binder类来实现，将str转成目标类型
+     *
+     * @param strValue 从request中获取的目标参数的string对象
+     * @return 转型后的目标值
+     */
     protected abstract T cast(String strValue);
 }
--- src/main/java/com/me/web/servlet/binding/UnsupportedTypeBinder.java
+++ src/main/java/com/me/web/servlet/binding/UnsupportedTypeBinder.java
@@ -4,7 +4,7 @@ package com.me.web.servlet.binding;
  * User: t.ding
  * Date: 13-1-11
  */
-class InvalidTypeBinder extends Binder<Object> {
+class UnsupportedTypeBinder extends Binder<Object> {
     @Override
     protected Object cast(String strValue) {
         return null;

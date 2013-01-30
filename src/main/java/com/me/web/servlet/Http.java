1a165976edf417ccf6c94b226389aef3d95f9b18
dingtao <d.t.ubest@gmail.com>
2013-01-30 19:10:34 +0800

    修改Http为一个class，强化不可实例性

--- src/main/java/com/me/web/servlet/Http.java
+++ src/main/java/com/me/web/servlet/Http.java
@@ -6,8 +6,11 @@ import java.util.regex.Pattern;
  * User: t.ding
  * Date: 13-1-15
  */
-public interface Http {
+public final class Http {
     public static final char uri_separator_char = '/';
     public static final String uri_separator = "/";
     public static final Pattern pathPattern = Pattern.compile("^/?[^?%+\\\\/#&]+(/[^?%+\\\\/#&]+)*/?$");
+
+    private Http() {
+    }
 }
